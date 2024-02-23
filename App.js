74c8ad9102106465c1d6ce1a04ed2162511a540a
Payhayal <begenay88@gmail.com>
2024-02-23 13:33:16 -0500

    input completed

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Alert,
+  ScrollView,
+} from 'react-native';
+import React, {useState} from 'react';
 import Header from './src/components/header';
-import styles from './src/components/header/style';
 import generalStyles from './src/utils/generalStyles';
-import Icon from 'react-native-vector-icons/dist/AntDesign';
+import Input from './src/components/input';
+import {colors} from './src/utils/constants';
 
 const App = () => {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    const newTodo = {
+      id: String(new Date().getTime()),
+      text: text,
+      date: new Date(),
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+    setText('');
+  };
+
   return (
-    <SafeAreaView style={generalStyles.flex1}>
+    <SafeAreaView style={[generalStyles.flex1, generalStyles.bgWhite]}>
       <Header title="My Todo App" />
-      <View>
-        <Text>week1 heyyy!!!</Text>
-        <Icon name="pluscircle" size={25} color="red" />
+      <Input
+        value={text}
+        onChangeText={text => setText(text)}
+        placeholder="Write..."
+        hasIcon
+        onIconPress={addTodo}
+      />
+      <View style={styles.todosWrapper}>
+        {todos.length === 0 ? (
+          <Text style={styles.emptyText}>You have no todos yet!</Text>
+        ) : (
+          <ScrollView>
+            <Text>ddfdfd</Text>
+          </ScrollView>
+        )}
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  todosWrapper: {
+    flex: 1,
+    marginHorizontal: 20,
+    marginVertical: 30,
+  },
+  emptyText: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: colors.textPrimary,
+  },
+  scrollView: {
+    flexGrow: 1,
+  },
+});
 
 export default App;

c029a93205df83aeb56e5f62a656da4d6dcd9a34
Payhayal <begenay88@gmail.com>
2024-02-22 20:37:19 -0500

    header added

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import {View, Text} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 import React from 'react';
+import Header from './src/components/header';
+import styles from './src/components/header/style';
+import generalStyles from './src/utils/generalStyles';
+import Icon from 'react-native-vector-icons/dist/AntDesign';
 
 const App = () => {
   return (
-    <View>
-      <Text>week1 heyyy!!!</Text>
-      <Text>This is my first commit in week 1</Text>
-    </View>
+    <SafeAreaView style={generalStyles.flex1}>
+      <Header title="My Todo App" />
+      <View>
+        <Text>week1 heyyy!!!</Text>
+        <Icon name="pluscircle" size={25} color="red" />
+      </View>
+    </SafeAreaView>
   );
 };
 
